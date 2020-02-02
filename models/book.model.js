431e5345ac0101c597884cc9a5bddc4cbc9f039c
Neoflies <tienloinguyen22@gmail.com>
2020-02-02 12:44:02 +0700

    Update book models

--- models/book.model.js
+++ models/book.model.js
@@ -13,6 +13,10 @@ const BookSchema = new mongoose.Schema({
   downloadUrl: String,
   referer: String,
   coverUrl: String,
+  downloadCount: {
+    type: Number,
+    default: 0,
+  },
 }, {
   timestamps: true,
 });
